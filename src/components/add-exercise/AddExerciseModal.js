268c5374f1fdefe75ee9d580c8ce476b36eacb97
brian_dobbs <bdobbs@sketchdev.io>
2022-01-05 08:23:15 -0600

    Add action and reducer folders, store for redux, and updates to AddExerciseModal to present a modal when the AddEsent modal when AddExerciseButton is pressed

--- src/components/add-exercise/AddExerciseModal.js
+++ src/components/add-exercise/AddExerciseModal.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import AddExerciseButton from "../navigation/AddExerciseButton";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 const AddExerciseModal = (props) => {
-  return <div></div>;
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <AddExerciseButton onClick={handleOpen} />
+    </div>
+  );
 };
 
 AddExerciseModal.propTypes = {};
